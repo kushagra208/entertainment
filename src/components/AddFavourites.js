6ce4b753afc4facef27e993a2a5ef8d957b89ec5
kushagra208 <kushagrag2082002@gmail.com>
2021-12-01 10:46:11 +0530

    first commit

---
+++ src/components/AddFavourites.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const AddFavourite = () => {
+	return (
+		<>
+			<span className='mr-2'>Add to Favourites</span>
+			<svg
+				width='1em'
+				height='1em'
+				viewBox='0 0 16 16'
+				/>
+		</>
+	);
+};
+
+export default AddFavourite;
---
+++ src/components/RemoveFavourites.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const RemoveFavourites = () => {
+	return (
+		<>
+			<span className='mr-2'>Remove from favourites</span>
+			<svg
+				width='1em'
+				height='1em'
+				viewBox='0 0 16 16'
+            />
+		</>
+	);
+};
+
+export default RemoveFavourites;
